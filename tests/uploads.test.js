565762af9184d2abd7d4083a8118979a1cd05380
yubi00 <ubrajkhadka@gmail.com>
2020-11-18 14:41:15 +1100

    test case modified for retrieving file

--- tests/uploads.test.js
+++ tests/uploads.test.js
@@ -25,6 +25,10 @@ test('Should upload a csv file', async () => {
 test('should retrive csv file by passing unique identifier as a query parameter', async () => {
   const id = Object.keys(store)[0];
   const response = await request(app).get(`/api/${id}`).expect(200);
+
+  expect(response.body).toHaveProperty('id');
+  expect(response.body).toHaveProperty('file');
+  expect(response.body).toHaveProperty('status');
 });
 
 test('should get average page view per day for different traffic type', async () => {
